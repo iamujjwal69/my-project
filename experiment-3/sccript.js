1e999437d06972ee9e5a91bb2e7f466e02da70c7
Ujjwal Sharma <163756996+iamujjwal69@users.noreply.github.com>
2025-08-12 15:35:19 +0530

    Delete experiment-3/sccript.js

--- experiment-3/sccript.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.getElementById("theme-toggle");
-const body = document.body;
-
-toggle.addEventListener("change", () => {
-  body.classList.toggle("dark-mode");
-  body.classList.toggle("light-mode");
-});
